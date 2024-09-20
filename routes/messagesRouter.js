9953889ecbfb1452c09924e483b6d704b44ce7a5
chryschi <121822197+chryschi@users.noreply.github.com>
2024-09-21 01:11:48 +0200

    Setup form on path /new for submitting message

--- routes/messagesRouter.js
+++ routes/messagesRouter.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 
 const messagesRouter = Router();
 
-messagesRouter.get("/", (req, res) => res.send("Here are the messages!"));
+messagesRouter.get("/", (req, res) => res.render("form"));
+messagesRouter.post("/", (req, res) => console.log("Message was posted!"));
 
 module.exports = messagesRouter;
