00caed0f32044f572523f0993a71582bd92cf153
chryschi <121822197+chryschi@users.noreply.github.com>
2024-09-21 01:41:43 +0200

    Redirect user to homepage after posting message

--- routes/messagesRouter.js
+++ routes/messagesRouter.js
@@ -4,8 +4,9 @@ const messages = require("../messages");
 const messagesRouter = Router();
 
 messagesRouter.get("/", (req, res) => res.render("form"));
-messagesRouter.post("/", (req, res) =>
-  messages.push({ text: req.body.message, user: req.body.authorName }),
-);
+messagesRouter.post("/", (req, res) => {
+  messages.push({ text: req.body.message, user: req.body.authorName });
+  res.redirect("/");
+});
 
 module.exports = messagesRouter;
