b2fee714bf3d56055dac33adf2b038f494e10aa0
chryschi <121822197+chryschi@users.noreply.github.com>
2024-09-21 00:57:07 +0200

    Display messages from users on index page

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,7 +1,22 @@
 const { Router } = require("express");
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => res.render("index"));
+const messages = [
+  {
+    text: "Hi and welcome!",
+    user: "Tom",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Dominique",
+    added: new Date(),
+  },
+];
+
+indexRouter.get("/", (req, res) =>
+  res.render("index", { title: "Mini Messageboard", messages: messages }),
+);
 indexRouter.get("*", (req, res) => res.status(404).send("ERROR"));
 
 module.exports = indexRouter;
